4662675caaa5fec9c6ccd094b0bd69776a745969
Cédric Marcone <cedric.marcone@valraiso.fr>
2024-06-10 13:37:56 +0200

    Refactor date formatting functions, improve grid interaction, and update slot styling in planning module

--- src/planning.tsx
+++ src/planning.tsx
@@ -1,8 +1,11 @@
-import classNames from "classnames";
+import * as React from "react";
 import * as Dates from "./lib/dates";
 import * as Arrays from "./lib/arrays";
 import * as Numbers from "./lib/numbers";
+import classNames from "classnames";
 import css from "./planning.module.css";
+
+type Day = keyof typeof Dates.DAYS;
 
 type Event = {
   type: string;
@@ -13,7 +16,7 @@ type Event = {
 
 type Props = {
   date: Date;
-  startDay: keyof typeof Dates.DAYS;
+  startDay: Day;
   startHour: number;
   endHour: number;
   events: Event[];
@@ -29,41 +32,32 @@ export default function WeekPlanning({
   const first = Dates.previousWeekday(date, startDay);
   const cols = Arrays.range(0, 7).map(Dates.formatDay(first));
   const rows = Arrays.range(startHour, endHour).map(Dates.formatHour);
-  let drag = false;
 
-  const gridMouseDown = (e: React.MouseEvent) => {
-    const target = e.target as HTMLElement;
-    if (target.classList.contains(css.slot)) return;
-
-    drag = true;
-    const grid = e.currentTarget;
-    grid.querySelectorAll(".slot").forEach((slot) => {
-      slot.setAttribute("inert", "true");
-    });
-    const ts = toTimestamp(e, first, startHour);
-    if (!ts) return;
-    console.log("down", ts);
-  };
-
-  const gridMouseUp = (e: React.MouseEvent) => {
-    const target = e.target as HTMLElement;
-    if (target.classList.contains(css.slot)) return;
-
-    drag = false;
-    const grid = e.currentTarget;
-    grid.querySelectorAll(".slot").forEach((slot) => {
-      slot.removeAttribute("inert");
-    });
-    const ts = toTimestamp(e, first, startHour);
-    if (!ts) return;
-    console.log("up  ", ts);
-  };
-
-  const gridMouseMove = (e: React.MouseEvent) => {
-    if (!drag) return;
+  const gridMouseMove = (e: MouseEvent) => {
     const ts = toTimestamp(e, first, startHour);
     if (!ts) return;
     console.log("move", ts);
+  };
+
+  const gridMouseUp = (e: MouseEvent) => {
+    document.removeEventListener("mousemove", gridMouseMove);
+
+    // if (targetsSlot(e)) return;
+    // const ts = toTimestamp(e, first, startHour);
+    // if (!ts) return;
+    // toggleSlotsInertia(e, false);
+    // console.log("up  ", ts);
+  };
+
+  const gridMouseDown = (e: React.MouseEvent) => {
+    // if (targetsSlot(e)) return;
+    const ts = toTimestamp(e, first, startHour);
+    if (!ts) return;
+    // toggleSlotsInertia(e, true);
+    console.log("down", ts);
+
+    document.addEventListener("mouseup", gridMouseUp, { once: true });
+    document.addEventListener("mousemove", gridMouseMove);
   };
 
   const slotClicked = (slot: Event) => (e: React.MouseEvent) => {
@@ -72,13 +66,7 @@ export default function WeekPlanning({
   };
 
   return (
-    <div
-      className={css.grid}
-      tabIndex={-1}
-      onMouseDown={gridMouseDown}
-      onMouseMove={gridMouseMove}
-      onMouseUp={gridMouseUp}
-    >
+    <div className={css.grid} tabIndex={-1} onMouseDown={gridMouseDown}>
       {mapEvents(events, startDay, startHour).map((slot, i) => {
         const { row, rowspan, col, colspan, title, type } = slot;
         const style = {
@@ -134,36 +122,38 @@ export default function WeekPlanning({
   );
 }
 
-function mapEvents(
-  events: Event[],
-  startDay: keyof typeof Dates.DAYS,
-  startHour: number
-) {
+function mapEvents(events: Event[], startDay: Day, startHour: number) {
   return events
-    .map((e) => {
-      const from = Dates.parseUTCTimestamp(e.from);
-      const to = Dates.parseUTCTimestamp(e.to);
-      const first = Dates.previousWeekday(from, startDay);
-
-      const row = from.getUTCHours() + 2 - startHour;
-      const col = Dates.differenceInDays(from, first) + 2;
-
-      const rowspan = to.getUTCHours() - from.getUTCHours();
-      const colspan = to.getUTCDate() - from.getUTCDate() + 1;
-
-      const hourInMinutes = rowspan * 60;
-      const startMinutes = from.getUTCMinutes();
-      const endMinutes = to.getUTCMinutes();
-      const minutes = hourInMinutes - startMinutes + endMinutes;
-      const size = Numbers.roundToDecimal((minutes / hourInMinutes) * 100, 2);
-      const start = Numbers.roundToDecimal((startMinutes / minutes) * 100, 2);
-
-      return { ...e, row, col, rowspan, colspan, size, start };
-    })
+    .map((e) => mapEvent(e, startDay, startHour))
     .sort((a, b) => (a.col === b.col ? a.row - b.row : a.col - b.col));
 }
 
-function toTimestamp(e: React.MouseEvent, first: Date, startHour: number) {
+function mapEvent(event: Event, startDay: Day, startHour: number) {
+  const from = Dates.parseUTCTimestamp(event.from);
+  const to = Dates.parseUTCTimestamp(event.to);
+  const first = Dates.previousWeekday(from, startDay);
+
+  const row = from.getUTCHours() + 2 - startHour;
+  const col = Dates.differenceInDays(from, first) + 2;
+
+  const rowspan = to.getUTCHours() - from.getUTCHours();
+  const colspan = to.getUTCDate() - from.getUTCDate() + 1;
+
+  const hourInMinutes = rowspan * 60;
+  const startMinutes = from.getUTCMinutes();
+  const endMinutes = to.getUTCMinutes();
+  const minutes = hourInMinutes - startMinutes + endMinutes;
+  const size = Numbers.roundToDecimal((minutes / hourInMinutes) * 100, 2);
+  const start = Numbers.roundToDecimal((startMinutes / minutes) * 100, 2);
+
+  return { ...event, row, col, rowspan, colspan, size, start };
+}
+
+function toTimestamp(
+  e: React.MouseEvent | MouseEvent,
+  first: Date,
+  startHour: number
+) {
   const target = e.target as HTMLElement;
   const ds = target.dataset;
   if (!ds.row || !ds.col) return;
@@ -183,3 +173,15 @@ function toTimestamp(e: React.MouseEvent, first: Date, startHour: number) {
 
   return Dates.formatUTCTimestamp(date);
 }
+
+// function targetsSlot(e: React.MouseEvent | MouseEvent) {
+//   const target = e.target as HTMLElement;
+//   return target.classList.contains(css.slot);
+// }
+
+// function toggleSlotsInertia(e: React.MouseEvent | MouseEvent, inert: boolean) {
+//   const grid = e.currentTarget as Element;
+//   grid.querySelectorAll(".slot").forEach((slot) => {
+//     inert ? slot.setAttribute("inert", "inert") : slot.removeAttribute("inert");
+//   });
+// }
